16a3c4ab59d7e44c14f3a76b830975e73ed4ffd3
khansameer25 <realskhan25@gmail.com>
2025-02-04 21:26:29 -0500

    Created account functionality

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
