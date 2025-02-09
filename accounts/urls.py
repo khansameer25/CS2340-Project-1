eacb2b51ee9545cc692603b9b6b3c1563d1c7560
Zan Bhatti <zanbhatti786@gmail.com>
2025-02-08 21:14:40 -0500

    chapter 12

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('signup', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
+    path('orders/', views.orders, name='accounts.orders'),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
