5c9b96cdb18332eb85a5ee09375f6d1648130744
khansameer25 <realskhan25@gmail.com>
2025-02-13 17:37:09 -0500

    Render movie in admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Movie, Review
+from django.utils.html import format_html
 
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
+
+    list_display = ['name']
+    readonly_fields = ['image_tag']
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" height="200px" />'.format(obj.image.url))
+    image_tag.short_description = 'Image'
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
