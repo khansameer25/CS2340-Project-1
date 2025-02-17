3818745fdda604000c4b3f29036225c727343994
khansameer25 <realskhan25@gmail.com>
2025-02-16 23:14:19 -0500

    Fixed admin page for creating users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
 from django import forms
@@ -10,10 +10,10 @@ class CustomErrorList(ErrorList):
             return ''
         return mark_safe(''.join([
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
-    
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -22,12 +22,41 @@ class CustomUserCreationForm(UserCreationForm):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         for fieldname in ['username', 'email', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update(
-                {'class': 'form-control'}
-            )
+            self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email already exists.")
+        return email
+
+class CustomUserChangeForm(UserChangeForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text=(
+            "Raw passwords are not stored, so there is no way to see this user's password, "
+            "but you can change the password using <a href=\"../password/\">this form</a>."
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field != 'password':
+                self.fields[field].help_text = None
+                self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
             raise forms.ValidationError("A user with this email already exists.")
         return email
