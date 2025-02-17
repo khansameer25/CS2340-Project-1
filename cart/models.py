472bec7b6706d18deeb64b0f98454935c161d53f
Yameen Ahmed <yameen.ahmed124@gmail.com>
2025-02-16 19:37:54 -0500

    updated admin perms for orders

--- cart/models.py
+++ cart/models.py
@@ -10,7 +10,30 @@ class Order(models.Model):
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
     def __str__(self):
-        return str(self.id) + ' - ' + self.user.username
+        return f"{self.id} - {self.user.username}"
+    
+    def add_movie(self, movie, quantity=1):
+        """ Adds a movie to the order or updates the quantity if it already exists. """
+        item, created = Item.objects.get_or_create(order=self, movie=movie, defaults={'price': movie.price, 'quantity': quantity})
+        if not created:
+            item.quantity += quantity
+            item.save()
+        self.update_total()
+
+    def remove_movie(self, movie):
+        """ Removes a movie from the order. """
+        try:
+            item = Item.objects.get(order=self, movie=movie)
+            item.delete()
+            self.update_total()
+        except Item.DoesNotExist:
+            pass
+
+    def update_total(self):
+        """ Updates the order total based on the current items. """
+        self.total = sum(item.price * item.quantity for item in self.item_set.all())
+        self.save()
+
     
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
